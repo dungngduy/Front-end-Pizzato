7539d8711e32d1bc98906910225a17aef09e366c
dungngduy <148855343+dungngduy@users.noreply.github.com>
2024-09-30 00:56:21 +0700

    update footer

--- src/pages/user/theme/Footer/index.js
+++ src/pages/user/theme/Footer/index.js
@@ -1,7 +1,145 @@
 import {memo} from "react";
+import 'assets/user/scss/home-page.scss';
+import {
+    AiOutlineFacebook,
+    AiOutlineInstagram,
+    AiOutlineTwitter,
+    AiOutlineYoutube,
+    AiOutlineFieldTime
+} from 'react-icons/ai';
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-    return <h1>Footer</h1>;
+    return (
+        <footer>
+            <div className="footer__container" style={{ backgroundImage: "url('/assets/images/footer.jpg')" }}>
+                <div className="overlay__footer">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-xl-3">
+                                <h2 className="footer__about">
+                                    Giới thiệu chung
+                                </h2>
+                                <p className="footer__description">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Konfugiendis blabla
+                                </p>
+                                <div className="footer__time">
+                                    <Link to={"/"}><AiOutlineFieldTime /></Link>
+                                    <div className="footer__hours">
+                                        <h4>Giờ mở cửa:</h4>
+                                        <p>Từ Thứ 2 - Chủ nhật <br />(08:00 - 22:00)</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xl-3">
+                                <h2 className="footer__about__me">
+                                    Về chúng tôi
+                                </h2>
+                                <div className="links__about__me">
+                                    <ul>
+                                        <li><Link to={"/"}>Trang chủ</Link></li>
+                                        <li><Link to={"/"}>Giới thiệu</Link></li>
+                                        <li><Link to={"/"}>Thực đơn</Link></li>
+                                        <li><Link to={"/"}>Bài viết</Link></li>
+                                        <li><Link to={"/"}>Liên hệ</Link></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="col-xl-3">
+                                <h2 className="footer__help">
+                                    Trợ giúp
+                                </h2>
+                                <div className="links__help">
+                                    <ul>
+                                        <li><Link to={"/"}>FAQ</Link></li>
+                                        <li><Link to={"/"}>Điều khoản & Bảo mật</Link></li>
+                                        <li><Link to={"/"}>Báo cáo</Link></li>
+                                        <li><Link to={"/"}>Tài liệu</Link></li>
+                                        <li><Link to={"/"}>Chính sách hỗ trợ</Link></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="col-xl-3">
+                                <h2 className="footer__post">
+                                    Bài viết gần đây
+                                </h2>
+                                <div className="recent__post">
+                                    <div className="recent__post__item">
+                                        <div className="recent__post__images">
+                                            <img src="/assets/images/recent-post-1.jpg" alt="" />
+                                        </div>
+                                        <div className="recent__post__text">
+                                            <p className="recent__post__title">
+                                                Đoạn văn bản 2 với nội dung dài hơn
+                                            </p>
+                                            <p className="recent__post__date">
+                                                12-12-2022
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className="recent__post__item">
+                                        <div className="recent__post__images">
+                                            <img src="/assets/images/recent-post-1.jpg" alt="" />
+                                        </div>
+                                        <div className="recent__post__text">
+                                            <p className="recent__post__title">
+                                                Đoạn văn bản 2 với nội dung dài hơn
+                                            </p>
+                                            <p className="recent__post__date">
+                                                12-12-2022
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div className="recent__post__item">
+                                        <div className="recent__post__images">
+                                            <img src="/assets/images/recent-post-1.jpg" alt="" />
+                                        </div>
+                                        <div className="recent__post__text">
+                                            <p className="recent__post__title">
+                                                Đoạn văn bản 2 với nội dung dài hơn
+                                            </p>
+                                            <p className="recent__post__date">
+                                                12-12-2022
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="footer__copyright">
+                <div className="container">
+                    <div className="row footer__copyright">
+                        <div className="col-xl-6 footer__copyright__text">
+                            <ul>
+                                <li>Copyright © 2024. All rights reserved</li>
+                            </ul>
+                        </div>
+                        <div className="col-xl-6">
+                            <div className="footer__copyright__icons">
+                                <ul>
+                                    <li>
+                                        <Link to={""}><AiOutlineFacebook /></Link>
+                                    </li>
+                                    <li>
+                                        <Link to={""}><AiOutlineInstagram /></Link>
+                                    </li>
+                                    <li>
+                                        <Link to={""}><AiOutlineYoutube /></Link>
+                                    </li>
+                                    <li>
+                                        <Link to={""}><AiOutlineTwitter /></Link>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
 };
 
 export default memo(Footer);
--- src/pages/user/theme/Header/index.js
+++ src/pages/user/theme/Header/index.js
@@ -1,4 +1,4 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 import 'assets/user/scss/home-page.scss';
 import { 
     AiOutlineFacebook,
@@ -6,13 +6,71 @@ import {
     AiOutlineTwitter,
     AiOutlineYoutube,
     AiOutlineMail,
-    AiOutlinePhone
+    AiOutlinePhone,
+    AiOutlineSearch,
+    AiOutlineUser,
+    AiOutlineShoppingCart
 } from 'react-icons/ai';
 import { Link } from "react-router-dom";
+import { ROUTER } from "utils/router";
 
 const Header = () => {
+    // Menu
+    const [Menus] = useState([
+        {
+            name: "Trang chủ",
+            path: ROUTER.USER.HOME,
+        },
+        {
+            name: "Giới thiệu",
+            path: ROUTER.USER.HOME,
+        },
+        {
+            name: "Thực đơn",
+            path: "",
+            isShowSubMenu: false,
+            child: [
+                {
+                    name: "Pasta",
+                    path: "",
+                },
+                {
+                    name: "Pizza",
+                    path: "",
+                },
+                {
+                    name: "Breakfast",
+                    path: "",
+                },
+                {
+                    name: "Desserts",
+                    path: "",
+                }
+            ]
+        },
+        {
+            name: "Bài viết",
+            path: ROUTER.USER.HOME,
+        },
+        {
+            name: "Liên hệ",
+            path: ROUTER.USER.HOME,
+        }
+    ]);
+
+    // Popup tìm kiếm món ăn
+    const [isSearchVisible, setSearchVisible] = useState(false);
+
+    const toggleSearchPopup = () => {
+        setSearchVisible(!isSearchVisible);
+    };
+
+    const closePopup = () => {
+        setSearchVisible(false);
+    };
+
     return (
-        <>
+        <header>
             <div className="header__top">
                 <div className="container">
                     <div className="row">
@@ -47,12 +105,65 @@ const Header = () => {
             </div>
             <div className="container">
                 <div className="row">
-                    <div className="col-xl-3 col-lg-3">LOGO</div>
-                    <div className="col-xl-6 col-lg-6">MENU</div>
-                    <div className="col-xl-3 col-lg-3">LOGIN</div>
+                    <div className="col-xl-3 col-lg-3">
+                        <div className="header__logo">
+                            <Link to={""}>
+                                <img src="/assets/images/logo-pizzato.webp" alt="Logo" />
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="col-xl-6 col-lg-6">
+                        <div className="header__menu">
+                            <ul>
+                                {
+                                    Menus.map((menu, index) => {
+                                        return (
+                                            <li key={index} className={index === 0 ? "active" : ""}>
+                                                <Link to={menu.path}>{menu.name}</Link>
+                                                {
+                                                    menu.child && (
+                                                        <ul className="header__menu__dropdown">
+                                                            {menu.child.map((childItem, childKey) => (
+                                                                <li key={`${index}-${childKey}`}>
+                                                                    <Link to={childItem.path}>{childItem.name}</Link>
+                                                                </li>
+                                                            ))}
+                                                        </ul>
+                                                    )
+                                                }
+                                            </li>
+                                        );
+                                    })
+                                }
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-xl-3 col-lg-3 search__container">
+                        <ul>
+                            <li onClick={toggleSearchPopup}><AiOutlineSearch /></li>
+                            <li><AiOutlineUser /></li>
+                            <li><AiOutlineShoppingCart /> <span>2</span></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </>
+
+            {/* Popup tìm kiếm */}
+            {isSearchVisible && (
+                <>
+                    {/* Lớp nền mờ */}
+                    <div className="overlay active" onClick={closePopup}></div>
+
+                    {/* Popup */}
+                    <div className="search-popup active">
+                        <div className="popup-content">
+                            <input type="text" placeholder="Tìm kiếm món ăn tại đây..." />
+                            <button onClick={closePopup}>Tìm kiếm</button>
+                        </div>
+                    </div>
+                </>
+            )}
+        </header>
     );
 };
 
