aa241d879d0745f21e6767589355e4f825e4200a
N6mnguyen <namsun3113@gmail.com>
2024-12-03 23:47:27 +0700

    bien the

--- src/pages/user/DetailPage/detail-product.jsx
+++ src/pages/user/DetailPage/detail-product.jsx
@@ -236,6 +236,8 @@ const DetailProduct = () => {
                                     ) : (
                                         null
                                     )}
+
+                                    {/* Tùy Chọn Viền */}
                                     {edges.length > 0 ? (
                                         edges.map((edge) => (
                                             <div className="mb-2">
