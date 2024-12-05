0633ec951473de6831d3c8319dd1ce7193a5c48d
dungngduy <148855343+dungngduy@users.noreply.github.com>
2024-12-05 21:51:18 +0700

    fix UI continue

--- src/pages/user/DetailPage/similar-product.jsx
+++ src/pages/user/DetailPage/similar-product.jsx
@@ -23,37 +23,39 @@ const SimilarProducts = () => {
             <h2 className="text-2xl font-bold mb-10" data-aos="fade-up">Sản phẩm tương tự</h2>
             <div className="grid grid-cols-4 gap-10" data-aos="fade-up">
                 {isSimilarPizza.map((pizza) => (
-                    <div key={pizza.id} className="relative group">
-                        {/* Product Image */}
-                        <img className="object-cover rounded-md" src={formatImage(pizza.thumb_image)} width={170} height={170} alt={pizza.thumb_image} />
+                    <Link to={`/detail/${pizza.id}`} key={pizza.id}>
+                        <div key={pizza.id} className="relative group">
+                            {/* Product Image */}
+                            <img className="object-cover rounded-md w-[180px] h-[180px]" src={formatImage(pizza.thumb_image)} alt={pizza.thumb_image} />
 
-                        {/* Overlay with Icons */}
-                        <div className="absolute mx-12 inset-y-px top-1 bg-white bg-opacity-0 group-hover:bg-opacity-10 transition-opacity duration-300 flex justify-center items-center gap-3 opacity-0 group-hover:opacity-100">
-                            <button className="bg-white p-2 rounded-full shadow-md hover:bg-yellow-400 transition">
-                                <FaHeart className="text-gray-600" />
-                            </button>
-                            <button className="bg-white p-2 rounded-full shadow-md hover:bg-yellow-400 transition">
-                                <FaExchangeAlt className="text-gray-600" />
-                            </button>
-                        </div>
+                            {/* Overlay with Icons */}
+                            <div className="absolute mx-12 inset-y-px top-1 bg-white bg-opacity-0 group-hover:bg-opacity-10 transition-opacity duration-300 flex justify-center items-center gap-3 opacity-0 group-hover:opacity-100">
+                                <button className="bg-white p-2 rounded-full shadow-md hover:bg-yellow-400 transition">
+                                    <FaHeart className="text-gray-600" />
+                                </button>
+                                <button className="bg-white p-2 rounded-full shadow-md hover:bg-yellow-400 transition">
+                                    <FaExchangeAlt className="text-gray-600" />
+                                </button>
+                            </div>
 
-                        {/* pizza Info */}
-                        <div className="mt-4 px-4">
-                            <div className="h-20 flex flex-col justify-between">
-                                <Link to={`/detail/${pizza.id}`}>
-                                    <h3 className="text-l font-bold">{pizza.name}</h3>
-                                    <div className='flex gap-2'>
-                                        <div className="text-yellow-600 text-x ">{formatCurrencyVND(pizza.offer_price)}</div>
-                                        <div>
-                                            {pizza.price && (
-                                                <div className="text-gray-500 line-through text-x">{formatCurrencyVND(pizza.price)}</div>
-                                            )}
+                            {/* pizza Info */}
+                            <div className="mt-4 px-4">
+                                <div className="h-20 flex flex-col justify-between">
+                                    <Link to={`/detail/${pizza.id}`}>
+                                        <h3 className="text-l font-bold">{pizza.name}</h3>
+                                        <div className='flex gap-2'>
+                                            <div className="text-yellow-600 text-x ">{formatCurrencyVND(pizza.offer_price)}</div>
+                                            <div>
+                                                {pizza.price && (
+                                                    <div className="text-gray-500 line-through text-x">{formatCurrencyVND(pizza.price)}</div>
+                                                )}
+                                            </div>
                                         </div>
-                                    </div>
-                                </Link>
+                                    </Link>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </Link>
                 ))}
             </div>
         </div>
